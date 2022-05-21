9464bbe235fb7b49e4414d70601e2b01acb574a3
iDevYT <alphangred57@gmail.com>
2022-05-21 11:41:42 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="The Profile Picture placeholer"/>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="The Profile placeholer"/>
       <p>{text}</p>
     </div>
   </>)
