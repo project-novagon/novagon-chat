189529aa420644ba691a3b143561337d9b574276
iDevYT <alphangred57@gmail.com>
2022-05-21 11:56:51 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt').limit(1000000000000000000000000000000);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
