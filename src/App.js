f9f1e8c62a149d53479c7d8b8b9ddfc25f56933b
iDevYT <alphangred57@gmail.com>
2022-05-21 15:15:06 +0200

    fujfnhiufh9uhz8

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     <div className="App">
       <header>
         <h1>iDevs Chat App</h1>
-        <a href='mailto://alphanged57@gmail.com'>Contact</a>
+        <a href='mailto://alphangred57@gmail.com'>Contact</a>
         <SignOut />
       </header>
 
