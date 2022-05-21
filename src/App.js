183568dffa3fd1f9f2759f7213f33183d63da7de
iDevYT <alphangred57@gmail.com>
2022-05-21 11:37:01 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
-import initApp from './APIKeys';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -8,8 +7,17 @@ import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-initApp();
-
+firebase.initializeApp({
+  apiKey: "AIzaSyANfFC73ZCDECWC7QnU-2DZmbnNwcYSiCQ",
+  authDomain: "idevs-chat-app.firebaseapp.com",
+  databaseURL: "https://idevs-chat-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "idevs-chat-app",
+  storageBucket: "idevs-chat-app.appspot.com",
+  messagingSenderId: "912253802832",
+  appId: "1:912253802832:web:ca09700061d4f1705ff455",
+  measurementId: "G-Q9CLKNEVMH"
+  
+});
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
