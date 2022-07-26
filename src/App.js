940db519f478cb718775c5c762a9acb99a2b3d4a
iDevYT <alphangred57@gmail.com>
2022-07-26 16:09:42 +0200

    ToS

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>Do not violate the <a href='./tos.js'>ToS</a> or you will be banned for life!</p>
     </>
   )
 
---
+++ src/tos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } 
+    from "./tos";
+  
+const TOS = () => {
+  return (
+    <><h1>iDev's Chat App ToS</h1>
+        <p>We are working on the ToS!</p></> 
+  );
+};
+  
+export default Navbar;
