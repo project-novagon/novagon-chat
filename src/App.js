6494eccbba5aad8544c464cf0c1103a3de4a3672
iDevYT <alphangred57@gmail.com>
2022-07-26 16:36:41 +0200

    tos

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the <a href='#'>ToS</a> or you will be banned for life!</p>
+      <p>Do not violate the ToS or you will be banned for life!</p>
     </>
   )
 
