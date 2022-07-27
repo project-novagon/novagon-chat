210d59e56eea21eea432287be1b3e89886f73d34
iDevYT <alphangred57@gmail.com>
2022-07-27 11:43:00 +0200

    a

--- src/App.js
+++ src/App.js
@@ -73,8 +73,9 @@ function SignIn() {
       <p>Do not violate the ToS or you will be banned for life!</p>
       <br/>
       <button className="link" onCllinick={() => SignUp = true}>E-mail Coming soon</button>
+      <signInWithEmail />
       <footer>
-      {SignUp ? <CreateInWithEmail /> : <signInWithEmail />}
+      <CreateInWithEmail />      
       </footer>
     </>
   )
